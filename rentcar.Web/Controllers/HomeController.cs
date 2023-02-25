9860721e950b5e6326c88d3523ac84d7f83ebf86
Daniel <109951979+danielcojocaru02@users.noreply.github.com>
2023-02-25 21:18:39 +0200

    Added 9 New Directories with functional links from homepage
    Added 9 New Directories with functional links from homepage

--- rentcar.Web/Controllers/HomeController.cs
+++ rentcar.Web/Controllers/HomeController.cs
@@ -23,5 +23,45 @@ namespace rentcar.Web.Controllers
         {
             return View();
         }
+        // GET: Home/About
+        public ActionResult About()
+        {
+            return View();
+        }
+        // GET: Home/Blogpost
+        public ActionResult Blogpost()
+        {
+            return View();
+        }
+        // GET: Home/Faq
+        public ActionResult Faq()
+        {
+            return View();
+        }
+        // GET: Home/Offers
+        public ActionResult Fleet()
+        {
+            return View();
+        }
+        // GET: Home/Team
+        public ActionResult Team()
+        {
+            return View();
+        }
+        // GET: Home/Terms
+        public ActionResult Terms()
+        {
+            return View();
+        }
+        // GET: Home/Testimonials
+        public ActionResult Testimonials()
+        {
+            return View();
+        }
+        // GET: Home/Blog
+        public ActionResult Blog()
+        {
+            return View();
+        }
     }
 }
