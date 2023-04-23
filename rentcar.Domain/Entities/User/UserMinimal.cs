6372961a81cc966ef6076c91817b94ac1f37d6e4
Daniel <109951979+danielcojocaru02@users.noreply.github.com>
2023-04-23 11:01:38 +0300

    Commit Implementing Business Logic

---
+++ rentcar.Domain/Entities/User/UserMinimal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using rentcar.Domain.Enums;
+
+namespace rentcar.Domain.Entities.User
+{
+     public class UserMinimal
+     {
+          public int Id { get; set; }
+          public string Username { get; set; }
+          public string Email { get; set; }
+          public DateTime LastLogin { get; set; }
+          public string LasIp { get; set; }
+          public URole Level { get; set; }
+     }
+}
