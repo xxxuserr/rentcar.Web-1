20902f584a437d4d069a5cbfe45ab210b6bf86bc
DimaNm08 <dumitrunimerenco@gmail.com>
2023-05-01 11:08:56 +0300

    Commit Login BLL+Database

--- rentcar.Web/Controllers/AccountsController.cs
+++ rentcar.Web/Controllers/AccountsController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
-using rentcar.Web.Models;
 using System.Linq;
 using rentcar.BusinessLogic;
 using rentcar.DataAccess;
@@ -13,9 +12,26 @@ namespace rentcar.Web.Controllers
 {
     public class AccountsController : Controller
     {
+        public ActionResult Login()
+        {
+            return View();
+        }
         public ActionResult Signup()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginBO objLoginBO)
+        {
+            if (ModelState.IsValid)
+            {
+                UserBL objLoginBl = new UserBL();
+                CustomBO objCustomBo = objLoginBl.Login(objLoginBO);
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "User or password is wrong");
+            return View(objLoginBO);
+
         }
         [HttpPost]
         public ActionResult SignUp(UserBO objUserBO)
