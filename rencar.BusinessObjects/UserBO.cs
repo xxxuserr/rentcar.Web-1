71d82037f5a357f2f7617c39d1aa0cae3b3aa1f2
Daniel <109951979+danielcojocaru02@users.noreply.github.com>
2023-05-01 08:37:09 +0300

    Commit BLL Signup Functional

---
+++ rencar.BusinessObjects/UserBO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace rentcar.BusinessObjects
+{
+    public class UserBO
+    {
+        public int UserId { get; set; }
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "User Name is required!")]
+        public string UserName { get; set; }
+        [Display(Name = "User Email")]
+        [Required(ErrorMessage = "User Email is required!")]
+
+        public string UserEmail { get; set; }
+        [Display(Name = "User Password")]
+        [Required(ErrorMessage = "User Password is required!")]
+
+        public string Password { get; set; }
+    }
+}
