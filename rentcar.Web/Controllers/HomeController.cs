8f1eb2bac402f858cecb5785d7924b5582594a33
Daniel <109951979+danielcojocaru02@users.noreply.github.com>
2023-05-08 20:25:28 +0300

    Added CFG For Roles
    Added CFG For Roles + a new layout, controller and a page dedicated only for administrator, where will be Admin Dashboard

--- rentcar.Web/Controllers/HomeController.cs
+++ rentcar.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using rentcar.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
---
+++ rentcar.Web/MyRoleProvider.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using rentcar.DataAccess;
+
+namespace rentcar.Web
+{
+    public class MyRoleProvider : RoleProvider
+    {
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+            using (var context = new UserDBEntities())
+            {
+                var res = (from user in context.Users
+                           join role in context.Roles on user.UserId equals role.UserId
+                           where user.UserName == username
+                           select role.Role1).ToArray();
+                return res;
+            }
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
