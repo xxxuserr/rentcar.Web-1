60752c84cb7a71f5e2bb6115a4e05a2b5069b83e
Daniel <109951979+danielcojocaru02@users.noreply.github.com>
2023-05-16 08:32:47 +0300

    Added Admin/Users page where existing users are listed

---
+++ rentcar.DataAccess/DbOperations/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using rentcar.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rentcar.DataAccess.DbOperations
+{
+    public class UserRepository
+    {
+        public int CreateUser(UserBO objUserBo)
+        {
+            using (var context = new UserDBEntities())
+            {
+                User emp = new User()
+                {
+                    UserName = objUserBo.UserName,
+                    UserEmail = objUserBo.UserEmail,
+
+                };
+                context.Users.Add(emp);
+                context.SaveChanges();
+                return emp.UserId;
+               
+                
+            }
+        }
+        public List<UserBO> GetUsers()
+        {
+            using (var context = new UserDBEntities()) 
+            {
+                var result = context.Users
+                    .Select(x => new UserBO()
+                    {
+                        UserId = x.UserId,
+                        UserName = x.UserName,
+                        UserEmail = x.UserEmail,
+                    }).ToList();
+                return result;
+            }
+        }
+    }
+}
--- rentcar.Web/Controllers/AdminController.cs
+++ rentcar.Web/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using rentcar.DataAccess.DbOperations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,21 @@ namespace rentcar.Web.Controllers
 {
     public class AdminController : Controller
     {
+        UserRepository repository = null;
+        public AdminController()
+        {
+            repository = new UserRepository();
+        }
         // GET: Admin
         [Authorize(Roles ="Admin")]
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Users()
+        {
+            var result = repository.GetUsers();
+            return View(result);
         }
     }
 }
