d3fee4b8c73436df0c3ba9bf77d99e1520c0c308
xxxuserr <spinu.ghita@gmail.com>
2023-05-25 22:17:15 +0300

    premium_class

--- rentcar.Web/Controllers/HomeController.cs
+++ rentcar.Web/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace rentcar.Web.Controllers
         {
             return View();
         }
+        // GET: Home/Offers_premium
+        public ActionResult Offers_premium()
+        {
+            return View();
+        }
         // GET: Home/About
         public ActionResult About()
         {
